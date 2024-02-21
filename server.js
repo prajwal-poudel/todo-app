1ee51f346b338226bf2d189409bd1d092d296093
prajwal-poudel <prajwalpoudel55@gmail.com>
2024-02-21 14:40:25 +0545

    register-user

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const http = require('http')
+const body_parser = require('body-parser')
 require('dotenv').config()
 var  port = process.env.PORT
 const app = express()
 
 const router = require('./routes')
+
+app.use(body_parser.urlencoded({extended:true}));
+app.use(body_parser.json())
 app.use("/",router)
 
 
